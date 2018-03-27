e107c6a956992809001dbee83de6198de3089db3
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-03-26 21:38:49 -0300

    Add some event listeners in javascript code

--- colorgame.js
+++ colorgame.js
@@ -8,7 +8,29 @@ var colors = [
 	];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
+	// initial colors to square
 	squares[i].style.backgroundColor = colors[i];
+
+	// add click listeners to squares
+	squares[i].addEventListener("click", function() {
+	
+	// grab color of clicked square
+	var clickedColor = this.style.backgroundColor;
+
+	// compare color to pickedColor
+	if (clickedColor === pickedColor) {
+		alert("Correct !");
+	}
+	else {
+		alert("Wrong !!!");
+	}
+
+	});
 }
+

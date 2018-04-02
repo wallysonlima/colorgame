0f85c345e2879ae0b9fde7724f9bda577e47b04f
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-04-02 18:27:33 -0300

    Add more functionalities

--- colorgame.js
+++ colorgame.js
@@ -5,6 +5,23 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+	// generate all new colors
+	colors = generateRandomColors(6);
+	// pick a new random color from array
+	pickedColor = pickColor();
+	// change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	// change colors of squares
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+
+	h1.style.backgroundColor = "#232323";
+});
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
@@ -20,6 +37,7 @@ for(var i = 0; i < squares.length; i++) {
 	// compare color to pickedColor
 	if (clickedColor === pickedColor) {
 		messageDisplay.textContent = "Correct!";
+		resetButton.textContent = "Play Again?";
 		changeColors(clickedColor);
 		h1.style.backgroundColor = clickedColor;
 	}

8eb6aa829ca2d6a5c4e71b67f378f755720dd586
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-07-18 18:11:59 -0300

    Refactor the code, improving the code

--- colorgame.js
+++ colorgame.js
@@ -6,9 +6,49 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
+for(var i = 0; i < modeButtons.length; i++) {
+	modeButtons[i].addEventListener("click", function(){
+		modeButtons[0].classList.remove("selected");
+		modeButtons[1].classList.remove("selected");
+		this.classList.add("selected");
+		this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+
+		reset();
+		//figure out how many squares to show
+		//pick new colors
+		//pick a new pickcolor
+		//update page to reflect changes
+
+	});
+}
+
+function reset() {
+	// generate all new colors
+	colors = generateRandomColors(numSquares);
+	// pick a new random color from array
+	pickedColor = pickColor();
+	// change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
+
+	// change colors of squares
+	for(var i = 0; i < squares.length; i++) {
+		if ( colors[i] ) {
+			squares[i].style.display = "block";
+			squares[i].style.background = colors[i];	
+		} else {
+			squares[i].style.display = "none";
+		}
+		
+	}
+
+	h1.style.backgroundColor = "steelblue";
+}
+
+/*
 easyBtn.addEventListener("click", function(){
 	hardBtn.classList.remove("selected");
 	easyBtn.classList.add("selected");
@@ -39,23 +79,9 @@ hardBtn.addEventListener("click", function(){
 		squares[i].style.display = "block";
 	}
 });
-
+*/
 resetButton.addEventListener("click", function() {
-	// generate all new colors
-	colors = generateRandomColors(numSquares);
-	// pick a new random color from array
-	pickedColor = pickColor();
-	// change colorDisplay to match picked Color
-	colorDisplay.textContent = pickedColor;
-	this.textContent = "New Colors";
-	messageDisplay.textContent = "";
-
-	// change colors of squares
-	for(var i = 0; i < squares.length; i++) {
-		squares[i].style.backgroundColor = colors[i];
-	}
-
-	h1.style.backgroundColor = "steelblue";
+	reset();
 });
 
 colorDisplay.textContent = pickedColor;
